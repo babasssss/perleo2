c9a9be306f336b72fee0d96fac49142b8e88b710
SOREAU BASTIEN <80333079+babasssss@users.noreply.github.com>
2023-06-10 13:41:22 +0200

    Scroll nav profil

--- resources/views/MonProfil.blade.php
+++ resources/views/MonProfil.blade.php
@@ -9,18 +9,40 @@
 
   <div class="base-layout">
     <div class="window-profile">
-      <div class="tab-bar ">
-        <div class="tab">
-           <a href="#" class="label-tab">Mes articles</a>
-        </div>
-        <div class="tab">
-           <a href="#" class="label-tab">Paramètres</a>
-        </div>
-        <div class="tab">
-           <a href="#" class="label-tab">Mes évènements</a>
+      <div class="scrollspy-container">
+        <div class="scrollspy-items">
+          <a href="#section1" class="scrollspy-item">Mes articles</a>
+          <a href="#section2" class="scrollspy-item">Mes évènements</a>
+          <a href="#section3" class="scrollspy-item">Paramètres</a>
         </div>
       </div>
-</div>
+    </div>
+  </div>
+  <div class="base-layout-articles">
+    sd
   </div>
 
+
 </x-content-layout>
+
+<script>
+  // Sélectionnez tous les éléments de navigation
+  const scrollspyItems = document.querySelectorAll('.scrollspy-item');
+
+  // Parcours des éléments et ajout d'un gestionnaire d'événement de clic à chacun
+
+  scrollspyItems.forEach(item => {
+    item.addEventListener('click', () => {
+      // Supprimez la classe active de tous les éléments
+      scrollspyItems.forEach(item => {
+        item.classList.remove('scrollspy-item-active');
+        item.classList.add('scrollspy-item');
+      });
+
+      // Ajoutez la classe active à l'élément cliqué
+      item.classList.add('scrollspy-item-active');
+      item.classList.remove('scrollspy-item');
+    });
+  });
+
+</script>
